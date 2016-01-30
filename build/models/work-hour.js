69e7585e6f7a70c8b2d4db0d81ba13dda2f0ebc4
Mohamad Jahani <m4m4lj@gmail.com>
2016-01-30 14:18:28 +0330

    fix(models): add missing WorkHour model file

---
+++ build/models/work-hour.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function (sequelize, Types) {
+  return sequelize.define('WorkHour', {
+    weekday: {
+      type: Types.ENUM('sat', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri'), // eslint-disable-line
+      allowNull: false
+    },
+    start: {
+      type: Types.TIME,
+      allowNull: false
+    },
+    end: {
+      type: Types.TIME,
+      allowNull: false
+    }
+  });
+};
