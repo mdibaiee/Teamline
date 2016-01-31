fd024177a9a854199cfa9fd89d12772e35a416da
Mohamad Jahani <m4m4lj@gmail.com>
2016-01-31 09:14:32 +0330

    feat(models): add recess model

--- src/models/recess.js
+++ src/models/recess.js
@@ -1,26 +1,36 @@
 module.exports = (sequelize, Types) =>
-  sequelize.define('Vacation', {
-    from: {
+  sequelize.define('Recess', {
+    name: {
+      type: Types.STRING
+    },
+    description: {
+      type: Types.TEXT
+    },
+    start: {
       type: Types.DATE,
       defaultValue: Types.NOW
     },
-    to: {
+    end: {
       type: Types.DATE,
       allowNull: false
+    },
+    status: {
+      type: Types.ENUM('pending', 'accepted', 'rejected'), // eslint-disable-line
+      defaultValue: 'pending'
     }
   }, {
     instanceMethods: {
       timeLeft() {
-        return new Date(this.to - this.from);
+        return new Date(this.end - this.start);
       }
     },
     scopes: {
       active: {
         where: {
-          from: {
+          start: {
             $lt: sequelize.fn('NOW')
           },
-          to: {
+          end: {
             $gt: sequelize.fn('NOW')
           }
         }
