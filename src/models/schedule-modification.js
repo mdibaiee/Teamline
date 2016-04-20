0d3d8b0175fa43648d02f0b394c725ebcc4a1460
Mahdi Dibaiee <mdibaiee@aol.com>
2016-04-20 10:41:18 +0430

    chore: update hapi-sequelize-crud

--- src/models/schedule-modification.js
+++ src/models/schedule-modification.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, Types) =>
     }
   }, {
     instanceMethods: {
-      timeLeft() {
+      duration() {
         return new Date(this.end - this.start);
       }
     },
